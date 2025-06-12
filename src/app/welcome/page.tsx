387c1c5e11864e940a896aed2e69531d6873621e
gabrielglp <gabriel_pg08@hotmail.com>
2025-06-12 14:03:57 -0300

    wip

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Button from "@/components/ui/Button";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -21,7 +20,7 @@ const WelcomePage = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
-        height: '100dvh' // Dynamic viewport height para Safari
+        height: '100dvh'
       }}
     >
       <div className="absolute inset-0" />
